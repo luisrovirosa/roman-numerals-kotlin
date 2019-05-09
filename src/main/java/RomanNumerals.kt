50b6ed0f293d66e2e8b379bf8b125f23fece8a96
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-09 16:59:40 +0200

    TPP 9: statement to tail recursion. refactor

--- src/main/java/RomanNumerals.kt
+++ src/main/java/RomanNumerals.kt
@@ -2,14 +2,12 @@ class RomanNumerals {
     fun convert(number: Int): String {
         val numbers = hashMapOf<Int, String>(
             1 to "I",
-            2 to "II",
-            3 to "III",
             4 to "IV"
         )
         if (numbers.contains(number)){
             return numbers.get(number)!!
         }
-        return ""
+        return "I" + convert(number-1)
     }
 
 
