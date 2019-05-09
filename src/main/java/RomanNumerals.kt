c4b46f77d513f74cd74b4019aba83febd0139303
Luis Rovirosa <luisrovirosa@gmail.com>
2019-05-09 17:09:28 +0200

    no transformation. #11

--- src/main/java/RomanNumerals.kt
+++ src/main/java/RomanNumerals.kt
@@ -5,7 +5,8 @@ class RomanNumerals {
             4 to "IV",
             5 to "V",
             9 to "IX",
-            10 to "X"
+            10 to "X",
+            11 to "XI"
         )
         if (numbers.contains(number)) {
             return numbers.get(number)!!
--- src/test/java/RomanNumeralsShould.kt
+++ src/test/java/RomanNumeralsShould.kt
@@ -14,7 +14,8 @@ class RomanNumeralsShould {
         "7, VII",
         "8, VIII",
         "9, IX",
-        "10, X"
+        "10, X",
+        "11, XI"
     )
     fun `return I for number 1`(number: Int, expectedValue: String){
         val converter = RomanNumerals()
